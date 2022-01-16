e591b940cf72ba85b70cf93d31502898ec410e15
jaimebaldin <you@example.com>
2022-01-16 19:13:57 -0300

    instalando o firebase e fazendo sua configuração

---
+++ src/firebaseConnection.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app';
+import'firebase/compat/app';
+import 'firebase/compat/firestore';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATnDooM-FQ2gOjzWb3VRB356z8XCCRYLA",
+    authDomain: "curso-eedf1.firebaseapp.com",
+    projectId: "curso-eedf1",
+    storageBucket: "curso-eedf1.appspot.com",
+    messagingSenderId: "391112091979",
+    appId: "1:391112091979:web:16e31b43426f423b67e0ce",
+    measurementId: "G-KKVBXQ0QGW"
+  };
+  
+  // Initialize Firebase
+if(!firebase.app.length){
+    const app = initializeApp(firebase);
+}
+
+export default firebase;  
