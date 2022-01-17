665d3bd747cbc81b0169df9e9be6920f9f031b44
jaimebaldin <you@example.com>
2022-01-16 21:34:37 -0300

    Criando os campos para preencher os dados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import firebase from './firebaseConnection';
 
 function App() {
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  function handleAdd(){
+    alert('clicou')
+  }
+
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <h1>Teste</h1><br/>
+
+      <label>Titulo: </label>
+      <textarea type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
+
+      <label>Autor: </label>
+      <textarea type="text" value={autor} onChange={(e) => setAutor(e.target.value)}/>
+
+      <button onClick={ handleAdd }> Cadastrar</button>
+
     </div>
   );
 }
